edd9d130cd8a2a5ba9b4735899a487ead32b14c6
Sandrasiinya <161156791+Sandrasiinya@users.noreply.github.com>
2026-06-05 13:27:52 +0700

    Add configuration for IOPN Testnet

---
+++ js/js/config.js
@@ -0,0 +1,14 @@
+const NETWORK = {
+ chainId: "0x22C8",
+ chainName: "IOPN Testnet",
+
+ rpcUrls: [
+   "https://rpc.iopn.network"
+ ],
+
+ nativeCurrency: {
+   name: "IOPN",
+   symbol: "IOPN",
+   decimals: 18
+ }
+};
